c5ab023151011c5cd034ca030ab2cc367e36e9d4
Olga <olg200919@ya.ru>
2022-05-28 21:21:09 +0300

    Дз задача 4

---
+++ Homework_task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine());
+int n = 1 ;
+for (int i = n; i <= m; i++)
+ {
+   if (i % 2 == 0) 
+  {
+   Console.Write(i + " ");
+  }
+ }
+ Console.WriteLine ( );
